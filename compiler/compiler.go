f298d34124298a416e29695decd369597415e325
silloi <silloi1993@gmail.com>
2022-07-02 21:28:14 +0900

    the smallest compiler

---
+++ compiler/compiler.go
@@ -0,0 +1,35 @@
+package compiler
+
+import (
+	"github.com/silloi/monkey/ast"
+	"github.com/silloi/monkey/code"
+	"github.com/silloi/monkey/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
