51781d762222f8fb734ca996b7cacebb3c7aac99
silloi <silloi1993@gmail.com>
2022-07-03 11:02:09 +0900

    infix expressions

--- code/code.go
+++ code/code.go
@@ -12,8 +12,14 @@ type Opcode byte
 
 const (
 	OpConstant Opcode = iota
+
 	OpAdd
+
 	OpPop
+
+	OpSub
+	OpMul
+	OpDiv
 )
 
 type Definition struct {
@@ -25,6 +31,9 @@ var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
 	OpAdd:      {"OpAdd", []int{}},
 	OpPop:      {"OpPop", []int{}},
+	OpSub:      {"OpSub", []int{}},
+	OpMul:      {"OpMul", []int{}},
+	OpDiv:      {"OpDiv", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
