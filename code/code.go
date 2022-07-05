10088346a20bd8fb6d30ee8cc8301864a2c06356
silloi <silloi1993@gmail.com>
2022-07-05 22:49:26 +0900

    adding the index operator

--- code/code.go
+++ code/code.go
@@ -42,6 +42,8 @@ const (
 	OpArray
 
 	OpHash
+
+	OpIndex
 )
 
 type Definition struct {
@@ -81,6 +83,8 @@ var definitions = map[Opcode]*Definition{
 	OpArray: {"OpArray", []int{2}},
 
 	OpHash: {"OpHash", []int{2}},
+
+	OpIndex: {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
--- compiler/compiler.go
+++ compiler/compiler.go
@@ -217,6 +217,20 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 
 		c.emit(code.OpHash, len(node.Pairs)*2)
+
+	case *ast.IndexExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Index)
+		if err != nil {
+			return err
+		}
+
+		c.emit(code.OpIndex)
+
 	}
 
 	return nil
