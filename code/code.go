5ce26f0c74b8816f962391d6acd28285693e7b29
silloi <silloi1993@gmail.com>
2022-07-08 22:09:49 +0900

    arguments

--- code/code.go
+++ code/code.go
@@ -95,7 +95,7 @@ var definitions = map[Opcode]*Definition{
 
 	OpIndex: {"OpIndex", []int{}},
 
-	OpCall: {"OpCall", []int{}},
+	OpCall: {"OpCall", []int{1}},
 
 	OpReturnValue: {"OpReturnValue", []int{}},
 
